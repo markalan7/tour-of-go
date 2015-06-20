975b6a3f412e22d4f24b2130e7e2d8fdd4d0e5b0
Mark Silvis <mas450@pitt.edu>
2015-06-20 15:12:50 -0400

    added webcrawler implementation. finished tour!

--- webcrawler/webcrawler.go
+++ webcrawler/webcrawler.go
@@ -2,6 +2,8 @@ package main
 
 import (
     "fmt"
+    "errors"
+    "sync"
 )
 
 type Fetcher interface {
@@ -10,29 +12,66 @@ type Fetcher interface {
     Fetch(url string) (body string, urls []string, err error)
 }
 
+var fetched = struct {
+    urls map[string]error
+    sync.Mutex
+}{urls: make(map[string]error)}
+
+var loading = errors.New("loading")
+
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
 func Crawl(url string, depth int, fetcher Fetcher) {
-    // TODO: Fetch URLs in parallel.
-    // TODO: Don't fetch the same URL twice.
-    // This implementation doesn't do either:
     if depth <= 0 {
         return
     }
-    body, urls, err := fetcher.Fetch(url)
-    if err != nil {
-        fmt.Println(err)
+
+    fetched.Lock()
+    if _, ok := fetched.urls[url]; ok {
+        fetched.Unlock()
+        fmt.Println("already fetched: ", url)
         return
     }
+    fetched.urls[url] = loading
+    fetched.Unlock()
+
+    body, urls, err := fetcher.Fetch(url)
+    fetched.Lock()
+    fetched.urls[url] = err
+    fetched.Unlock()
+
+    if err != nil {
+        fmt.Printf("%v failed: %v\n", url, err)
+        return
+    }
+
+    done := make(chan bool)
     fmt.Printf("found: %s %q\n", url, body)
     for _, u := range urls {
-        Crawl(u, depth-1, fetcher)
+        fmt.Printf("Crawling child of %v: %v\n", url, u)
+        go func(url string) {
+            Crawl(url, depth-1, fetcher)
+            done<-true
+        }(u)
     }
-    return
+    fmt.Printf("Done with %v\n", url)
+    for _, u := range urls {
+        fmt.Printf("Waiting for %v\n", u)
+        <-done
+    }
 }
 
 func main() {
     Crawl("http://golang.org/", 4, fetcher)
+
+    fmt.Println("\nFetching")
+    for url, err := range fetched.urls {
+        if err != nil {
+            fmt.Printf("error fetching: %v\n", err)
+        } else {
+            fmt.Printf("%v fetched\n", url)
+        }
+    }
 }
 
 // fakeFetcher is Fetcher that returns canned results.
