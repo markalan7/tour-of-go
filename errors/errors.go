66cdd1ac0e6fb7dd9f7e568e7226ae48a57573f3
Mark Silvis <mas450@pitt.edu>
2015-06-19 21:58:38 -0400

    small update

--- errors/errors.go
+++ errors/errors.go
@@ -7,15 +7,15 @@ import (
 
 type ErrNegativeSqrt float64
 
+func (e ErrNegativeSqrt) Error() string {
+    return fmt.Sprintf("cannot Sqrt negative number: %g", float64(e))
+}
+
 func Sqrt(x float64) (float64, error) {
     if x < 0 {
         return 0, ErrNegativeSqrt(x)
     }
     return math.Sqrt(x), nil
-}
-
-func (e ErrNegativeSqrt) Error() string {
-    return fmt.Sprintf("cannot Sqrt negative number: %g", float64(e))
 }
 
 func main() {
